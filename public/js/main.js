81f5308317dc823c56e8dd188b4bd5d0c79ff216
Leandre <leandre.b.boislard@gmail.com>
2023-05-11 22:26:38 -0400

    js pour bug fix nav

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,9 @@ const hamburger = document.querySelector(".hamburger");
 
 const qualiStarter = document.querySelector(".start--animation");
 const qualiDriver = document.querySelector(".driver_circle");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    const navMenu = document.querySelector(".nav-menu");
     console.log("Click ");
 
     if (hamburger.classList.contains("open-hamburger")) {
@@ -22,31 +22,34 @@ hamburger.addEventListener("click", () => {
     }
 });
 
-qualiStarter.addEventListener("click", () => {
+if (qualiStarter) {
+    qualiStarter.addEventListener("click", () => {
+        if (qualiDriver.classList.contains("ended")) {
+            qualiDriver.classList.remove("driver_circle");
+            setTimeout(() => {
+                qualiDriver.classList.remove("ended");
+                qualiDriver.classList.add("driver_circle");
+            }, 0);
+        } else if (!qualiDriver.classList.contains("pause")) {
+            qualiDriver.classList.add("pause");
+            qualiStarter.innerHTML = "Démarrer";
+        } else {
+            qualiDriver.classList.remove("pause");
+            qualiStarter.innerHTML = "Pause";
+        }
+    });
 
+    qualiDriver.addEventListener("animationend", () => {
+        qualiDriver.classList.add("ended");
+        qualiStarter.innerHTML = "Redémarré";
+    });
+}
 
-
-    if (qualiDriver.classList.contains("ended")) {
-
-        qualiDriver.classList.remove("driver_circle")
-        setTimeout(() => {
-            qualiDriver.classList.remove("ended");
-            qualiDriver.classList.add("driver_circle");
-        }, 0);
-
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+        hamburger.classList.remove("open-hamburger");
+        hamburger.classList.add("closed-hamburger");
+        navMenu.classList.remove("nav-open");
+        navMenu.classList.add("nav-closed");
     }
-    else if (!qualiDriver.classList.contains("pause")) {
-        qualiDriver.classList.add("pause");
-        qualiStarter.innerHTML = "Démarrer";
-    } else {
-        qualiDriver.classList.remove("pause");
-        qualiStarter.innerHTML = "Pause";
-    }
-
-
-});
-
-qualiDriver.addEventListener("animationend", () => {
-    qualiDriver.classList.add("ended");
-    qualiStarter.innerHTML = "Redémarré";
 });
