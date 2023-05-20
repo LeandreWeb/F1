c62c9795b4df06c61768a79967b4b0e2d6b99fcc
Leandre <leandre.b.boislard@gmail.com>
2023-05-20 11:40:14 -0400

    Ajout Status dans table Grand_Prix

--- database/migrations/2023_03_26_214834_create_grand_prix_weekends_table.php
+++ database/migrations/2023_03_26_214834_create_grand_prix_weekends_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('grand_prix_weekends', function (Blueprint $table) {
             $table->id();
             $table->string("name")->unique();
+            $table->string("status");
             $table->foreignId('country_id')->constrained()->onUpdate("cascade")->onDelete("cascade");
             $table->foreignId('race_id')->constrained()->onUpdate("cascade")->onDelete("cascade");
             $table->foreignId('qualification_id')->constrained()->onUpdate("cascade")->onDelete("cascade");
