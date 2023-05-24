c7e60169e80f2e3e4f2af9a24391385a5dcba3e2
Leandre <leandre.b.boislard@gmail.com>
2023-05-23 21:41:30 -0400

    Ajour du prmier path

--- resources/views/Schedule/schedule.blade.php
+++ resources/views/Schedule/schedule.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-<main class="in_construction">
-    <h1 >Calendrier</h1>
-    @include('.components.constuction')
-</main>
-@endsection
+    <main class="in_construction">
+        <h1>Calendrier</h1>
+        @include('.components.constuction')
 
+
+        @include('.svg.CalendarTracks.Circuit International de Bahrain')
+        @include('.svg.CalendarTracks.Circuit de la corniche de Djeddah')
+
+    </main>
+@endsection
