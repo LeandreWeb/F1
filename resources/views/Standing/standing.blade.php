5232e939f772a39059501f25adf54e1868f3d757
Leandre <leandre.b.boislard@gmail.com>
2023-06-02 18:38:29 -0400

    Boucle pour les teams

--- resources/views/Standing/standing.blade.php
+++ resources/views/Standing/standing.blade.php
@@ -3,9 +3,11 @@
 @section('content')
 <main class="in_construction">
     <h1>Classement</h1>
-    @foreach ( $teams as $team )
-        <h3>{{$team->points}}</h3>
-    @endforeach
+    @for ($position = 1; $position <count($teams)+1 ; $position++)
+        <h3>{{$position}}<h3>
+
+
+    @endfor
 </main>
 
 @endsection
