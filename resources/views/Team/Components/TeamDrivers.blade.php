f73c46e38a81f1c7382356144ddf7c88512da2b7
Leandre <leandre.b.boislard@gmail.com>
2023-06-18 13:23:13 -0400

    feat: integration of drivers in team page

--- resources/views/Team/Components/TeamDrivers.blade.php
+++ resources/views/Team/Components/TeamDrivers.blade.php
@@ -1,4 +1,7 @@
 <section class="team__drivers__container">
+  <h2 class="team__drivers--titles team_{{$team->id}}--text">
+    Pilotes
+  </h2>
     @foreach ($team->drivers as $driver)
         <div class="team__driver__card__container ">
             <img src="{{ asset('Images/Drivers/Side-profil/' . $driver->Lastname . '.avif') }}" alt=""
@@ -11,9 +14,10 @@
                 <div class="team__driver--lastname team__driver__info--container">Nom de Famille: <span
                         class="team_{{ $driver->team->id }}--text">{{ $driver->Lastname }}</span></div>
                 <div class="team__driver--country team__driver__info--container">Pays: <span
-                        class="team_{{ $driver->team->id }}--text">{{ $driver->country->name }}</span></div>
+                        class="team_{{ $driver->team->id }}--text">{{ $driver->country->name }}</span><span><img src="{{asset('Images/Flags/'.$driver->country->name.'.jpg')}}" alt=""></span></div>
                 <div class="team__driver__infos--line team_{{ $driver->team->id }}--background"></div>
             </div>
+            <img src="{{asset('Images/Drivers/Number/'.$driver->Lastname.'.avif')}}" alt="" class="team__driver--number">
         </div>
     @endforeach
 
@@ -21,24 +25,29 @@
 
 
 <style>
+
     .team__drivers__container {
         height: 100vh;
         display: flex;
         flex-direction: column;
         padding: 0 4rem;
         justify-content: space-evenly;
+        background-color: var(--background-dark);
+        align-items: center;
+    }
 
-
+    .team__drivers--titles{
+      font-size: 6.4rem;
+      flex: .3 0 auto;
+      font-weight: 900;
     }
 
     .team__driver__card__container {
-
+        flex: 1 0 auto;
         display: flex;
-        padding: 0 2rem;
-        border-top: transparent;
-        border-left: transparent;
-        border-right: transparent;
-        box-shadow: none;
+        padding: 10rem;
+        justify-content: center;
+
     }
 
     .team__driver__card__container:hover,
@@ -49,9 +58,9 @@
 
     .team__driver__photo {
         aspect-ratio: 1/1;
-        align-self: center;
-        height: 100%;
-        max-height: 35rem;
+        align-self: flex-end;
+        width: 25rem;
+
     }
 
     .team__driver__infos {
@@ -62,6 +71,7 @@
         grid-template-columns: repeat(2, 1fr);
         grid-row-gap: 1rem;
         grid-column-gap: 2rem;
+        flex: 0 0 80rem;
     }
 
     .team__driver__info--container {
@@ -77,6 +87,11 @@
 
         transition: .75s;
         font-weight: 800;
+    }
+    .team__driver__info--container span img {
+        height: 2.5rem;
+        margin-left: 1rem;
+
     }
 
     .visible_driver .team__driver__info--container span {
@@ -97,5 +112,11 @@
 
       width: 100%;
 
+    }
+
+    .team__driver--number{
+      aspect-ratio: 183/103;
+      align-self: flex-end;
+      width: 20rem;
     }
 </style>
