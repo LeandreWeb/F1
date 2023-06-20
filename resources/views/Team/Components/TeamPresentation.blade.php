0f59086472426d966301132ace2d3de0a7b0ac8e
Leandre <leandre.b.boislard@gmail.com>
2023-06-20 19:43:07 -0400

    encjh: changer emplacement presentations

--- resources/views/Team/Components/TeamPresentation.blade.php
+++ resources/views/Team/Components/TeamPresentation.blade.php
@@ -8,6 +8,14 @@
 </section>
 
 <style>
+    .team__presentation__container{
+        width: 100%;
+        height: 100vh;
+        display: grid;
+        place-items: center;
+        position: relative;
+        overflow: hidden;
+    }
     .team__presentation__name__container{
         position: absolute;
         width: fit-content;
--- resources/views/Team/TeamPage.blade.php
+++ resources/views/Team/TeamPage.blade.php
@@ -7,14 +7,7 @@
     @include('Team.Components.TeamDrivers',compact("team"))
 </main>
 <style>
-    .team__presentation__container{
-        width: 100%;
-        height: 100vh;
-        display: grid;
-        place-items: center;
-        position: relative;
-        overflow: hidden;
-    }
+    
 
 </style>
 
