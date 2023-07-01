84a6ee8c23818569220be48a9632e05b21752ece
Leandre <leandre.b.boislard@gmail.com>
2023-07-01 18:58:58 -0400

    bugfix: change the 100vh pour 100dvh (voir si sa foncyionne)

--- resources/views/Team/Components/TeamInfos.blade.php
+++ resources/views/Team/Components/TeamInfos.blade.php
@@ -43,7 +43,7 @@
 <style>
     .team__infos--wrapper {
         width: 100%;
-        height: 100vh;
+        height: 100dvh;
         padding: 2rem 4rem 4rem 4rem;
         display: flex;
         flex-direction: column;
