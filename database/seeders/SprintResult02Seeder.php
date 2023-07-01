27b3b0d25e4b30c6b951009efc33800669e41c4f
Leandre <leandre.b.boislard@gmail.com>
2023-07-01 11:26:11 -0400

    feat: texte + positions

--- database/seeders/SprintResult02Seeder.php
+++ database/seeders/SprintResult02Seeder.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 
-class SprintResult01Seeder extends Seeder
+class SprintResult02Seeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -19,7 +19,7 @@ class SprintResult01Seeder extends Seeder
 
         $sprintResults[]=[
             "id"=>1001,
-            "driver_id"=>11,
+            "driver_id"=>1,
             "sprint_id"=>10,
             "total_time"=>"00:33:17.667",
             "position"=>1,
@@ -27,7 +27,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=>1002,
-            "driver_id"=>16,
+            "driver_id"=>11,
             "sprint_id"=>10,
             "total_time"=>"00:33:22.130",
             "position"=>2,
@@ -35,7 +35,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=>1003,
-            "driver_id"=>1,
+            "driver_id"=>55,
             "sprint_id"=>10,
             "total_time"=>"00:33:22.732",
             "position"=>3,
@@ -43,7 +43,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=>1004,
-            "driver_id"=>63,
+            "driver_id"=>18,
             "sprint_id"=>10,
             "total_time"=>"00:33:26.199", //+48.052
             "position"=>4,
@@ -51,7 +51,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=>1005,
-            "driver_id"=>55,
+            "driver_id"=>14,
             "sprint_id"=>10,
             "total_time"=>"00:33:28.055", //+50.977
             "position"=>5,
@@ -59,7 +59,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=>1006,
-            "driver_id"=>14,
+            "driver_id"=>27,
             "sprint_id"=>10,
             "total_time"=>"00:33:29.280", //+54.502
             "position"=>6,
@@ -67,7 +67,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=>1007,
-            "driver_id"=>44,
+            "driver_id"=>31,
             "sprint_id"=>10,
             "total_time"=>"00:33:34.170", //+55.873
             "position"=>7,
@@ -75,7 +75,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1008,
-            "driver_id"=>18,
+            "driver_id"=>63,
             "sprint_id"=>10,
             "total_time"=>"00:33:35.084", //+72.647
             "position"=>8,
@@ -83,7 +83,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1009,
-            "driver_id"=>23,
+            "driver_id"=>4,
             "sprint_id"=>10,
             "total_time"=>"00:33:38.424", //+73.753
             "position"=>9,
@@ -91,7 +91,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1010,
-            "driver_id"=>81,
+            "driver_id"=>44,
             "sprint_id"=>10,
             "total_time"=>"00:33:40.518", //+89.774
             "position"=>10,
@@ -99,7 +99,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1011,
-            "driver_id"=>20,
+            "driver_id"=>81,
             "sprint_id"=>10,
             "total_time"=>"00:33:45.657", //+90.870
             "position"=>11,
@@ -107,7 +107,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1012,
-            "driver_id"=>24,
+            "driver_id"=>16,
             "sprint_id"=>10,
             "total_time"=>"00:33:52.269", //+1
             "position"=>12,
@@ -115,7 +115,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1013,
-            "driver_id"=>10,
+            "driver_id"=>23,
             "sprint_id"=>10,
             "total_time"=>"00:33:54.586", //+1
             "position"=>13,
@@ -123,7 +123,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1014,
-            "driver_id"=>21,
+            "driver_id"=>20,
             "sprint_id"=>10,
             "total_time"=>"00:34:01.254", //+1
             "position"=>14,
@@ -131,7 +131,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1015,
-            "driver_id"=>27,
+            "driver_id"=>10,
             "sprint_id"=>10,
             "total_time"=>"00:34:06.254", //+1
             "position"=>15,
@@ -139,7 +139,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1016,
-            "driver_id"=>77,
+            "driver_id"=>22,
             "sprint_id"=>10,
             "total_time"=>"00:34:07.584", //+1
             "position"=>16,
@@ -147,7 +147,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1017,
-            "driver_id"=>4,
+            "driver_id"=>21,
             "sprint_id"=>10,
             "total_time"=>"00:34:09.771", //+2
             "position"=>17,
@@ -155,7 +155,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1018,
-            "driver_id"=>31,
+            "driver_id"=>2,
             "sprint_id"=>10,
             "total_time"=>"00:34:18.288", //+1
             "position"=>null,
@@ -163,7 +163,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1019,
-            "driver_id"=>22,
+            "driver_id"=>24,
             "sprint_id"=>10,
             "total_time"=>null, //+1
             "position"=>null,
@@ -171,7 +171,7 @@ class SprintResult01Seeder extends Seeder
         ];
         $sprintResults[]=[
             "id"=> 1020,
-            "driver_id"=>2,
+            "driver_id"=>77,
             "sprint_id"=>10,
             "total_time"=>null, //+1
             "position"=>null,
--- database/seeders/SprintStorySeeder.php
+++ database/seeders/SprintStorySeeder.php
@@ -31,10 +31,20 @@ class SprintStorySeeder extends Seeder
         ];
         $SprintStory[]=[
             "id"=>10,
-            "catchphrase"=>"",
-            "first_part"=>"",
-            "last_part"=>"",
-            "conclusion"=>"",
+            "catchphrase"=>"Pluie et Stratégies",
+            "intro"=>"Alors que nous nous préparons pour une course pleine de rebondissements, les enjeux sont élevés pour certains pilotes qui devront effectuer une remontée spectaculaire. Parmi eux, George Russell et Lewis Hamilton, qui ont du travail à faire pour se frayer un chemin à travers le peloton.
+            De plus, la météo a décidé de se mêler à la compétition en apportant la pluie sur le circuit autrichien. Cette variable imprévisible ajoute une dose supplémentaire d'incertitude et de complexité à la course. Toute la grille de départ a opté pour des pneus intermédiaires, à l'exception de Valtteri Bottas qui a choisi les pneus mediums, le plaçant à la 19e position sur la grille.",
+            "first_part"=>"
+            La première partie du Sprint Shootout en Autriche a débuté de manière spectaculaire avec un départ impressionnant de Sergio Perez. Le pilote Red Bull a réussi à dépasser Max Verstappen pour prendre la tête de la course. Cependant, sa joie fut de courte durée, car Verstappen a rapidement riposté et repris la première position au virage 3. Mais les surprises ne s'arrêtent pas là.
+            Au virage 6, Nico Hülkenberg, qui a réalisé une remarquable performance, a réussi à dépasser Perez pour s'emparer de la deuxième place. Les dépassements et les batailles intenses ont été le point d'orgue de cette première partie du Sprint Shootout. Une bataille féroce pour la 9e position a opposé Lando Norris à Charles Leclerc, tandis que Kevin Magnussen s'est disputé la 11e position avec les deux pilotes McLaren.
+            Au tour 12, Perez a fait preuve d'une grande détermination et a réussi à rattraper Hülkenberg, reprenant ainsi la deuxième position. La lutte pour les premières positions reste donc ouverte, avec une compétition acharnée entre les pilotes qui ne reculent devant rien pour gagner des places et se rapprocher du leader, Max Verstappen.",
+            "last_part"=>"
+            Dans la dernière partie du Sprint Shootout en Autriche, la piste commence à sécher progressivement, ce qui ajoute une nouvelle dynamique à la course. Au tour 15, Lando Norris parvient enfin à dépasser Charles Leclerc pour s'emparer de la 9e position, profitant d'une erreur de ce dernier au dernier virage. Les pilotes rivalisent ardemment pour gagner chaque position disponible.
+            Au tour 17, George Russell décide de rentrer aux stands pour chausser des pneus tendres, effectuant ainsi un changement stratégique. Il en profite pour réaliser le meilleur tour de piste, ce qui incite plusieurs autres pilotes à suivre son exemple et à effectuer un arrêt aux stands pour changer de pneus. Parmi eux, Nico Hülkenberg parvient à rattraper les pilotes qui n'ont pas effectué de changement de pneus, lui permettant de sécuriser la 6e position. Russell, quant à lui, réalise également une impressionnante remontée, manquant de peu de ravir la 7e place à Esteban Ocon, avec seulement 0.009 seconde d'écart.
+            Malgré les événements se déroulant en arrière-plan du classement, Max Verstappen confirme sa domination en remportant facilement le Sprint. Sa maîtrise et sa vitesse ont été remarquables tout au long de la course, lui permettant de franchir la ligne d'arrivée en première position sans être inquiété.",
+            "conclusion"=>"En conclusion, le Sprint Shootout en Autriche a offert un spectacle intense et captivant aux amateurs de Formule 1. Les pilotes ont fait face à des défis variés, allant des conditions météorologiques changeantes à la gestion stratégique des pneus. Malgré les rebondissements et les batailles serrées tout au long de la course, c'est Max Verstappen qui a brillé en remportant une victoire dominante.
+            Les performances exceptionnelles de Verstappen soulignent sa maîtrise et sa détermination à maintenir sa position de leader. Cependant, il ne faut pas négliger les prouesses des autres pilotes qui ont rivalisé avec ténacité pour gagner des places et se frayer un chemin vers le haut du classement.
+            Le Sprint Shootout en Autriche a également révélé l'importance des stratégies et des décisions tactiques dans la course. Les arrêts aux stands opportuns et les choix de pneus ont joué un rôle crucial dans la performance des pilotes. Les dépassements audacieux et les batailles acharnées ont créé une atmosphère palpitante et imprévisible tout au long de la course.",
             "extra"=>null,
         ];
 
