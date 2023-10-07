054cf56efb0a66735cc8c095766feb4dd2690537
Leandre <leandre.b.boislard@gmail.com>
2023-10-07 17:21:20 -0400

    Change the conditions for the links

--- resources/views/Schedule/Components/event--done.blade.php
+++ resources/views/Schedule/Components/event--done.blade.php
@@ -7,24 +7,33 @@
             <h3>{{ $gp->name }}</h3>
         </div>
         <div class="event_info_events">
-            <a href="{{ route('qualificationNews', ['id' => $gp->id]) }}">Qualification</a>
+            @if ($gp->qualification->qualification_story_id)
+                <a href="{{ route('qualificationNews', ['id' => $gp->id]) }}">Qualification</a>
+            @else            
+                <div class="article_to_do">Qualification</div>
+            @endif
             @if ($gp->sprint_shootout_id)
-                <a href="{{ route('sprint shootoutNews', ['id' => $gp->id]) }}">Sprint Shootout</a>
+                @if ($gp->sprintShootout->sprint_shootout_story_id)
+                    <a href="{{ route('sprint shootoutNews', ['id' => $gp->id]) }}">Sprint Shootout</a>
+                @else            
+                    <div class="article_to_do">Sprint Shootout</div>
+                @endif
             @endif
             @if ($gp->sprint)
-                <a href="{{ route('sprintNews', ['id' => $gp->id]) }}">Sprint</a>
+                @if ($gp->sprint->sprint_story_id)
+                    <a href="{{ route('sprintNews', ['id' => $gp->id]) }}">Sprint</a>
+                @else            
+                    <div class="article_to_do">Sprint</div>
+                @endif
             @endif
-            <a href="{{ route('raceNews', ['id' => $gp->id]) }}">Course</a>
-
-
+            @if ($gp->race->race_story_id)
+                <a href="{{ route('raceNews', ['id' => $gp->id]) }}">Course</a>
+            @else            
+                <div class="article_to_do">race</div>
+            @endif
         </div>
-
         <h2 class="event_round">Ronde {{ $gp->id }}</h2>
-
-
     </div>
-
-
 </div>
 
 <style>
