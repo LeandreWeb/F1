4ac779f5821d1ef31e7ed0a2577104f7b352d38c
Leandre <leandre.b.boislard@gmail.com>
2024-02-21 21:28:40 -0500

    Side Profils done

--- resources/views/Drivers/Components/Driver.blade.php
+++ resources/views/Drivers/Components/Driver.blade.php
@@ -1,19 +1,27 @@
-
-<div class="driver-card team_{{$teamdriver->seasonTeam->team->id}}--border"">
+<div class="driver-card team_{{ $teamdriver->seasonTeam->team->id }}--border"">
     <div class="driver-info">
-        <div class="driver-number team_{{$teamdriver->seasonTeam->team->id}}--text"># {{$teamdriver->driver->id}}</div>
+        <div class="driver-number team_{{ $teamdriver->seasonTeam->team->id }}--text"># {{ $teamdriver->driver->id }}
+        </div>
         <div class="driver-fullname">
-            <h4 class="driver-firstname">{{$teamdriver->driver->Firstname}}</h4>
-            <h3 class= "driver-lastname team_{{$teamdriver->seasonTeam->team->id}}--text">{{$teamdriver->driver->Lastname}}</h3>
+            <h4 class="driver-firstname">{{ $teamdriver->driver->Firstname }}</h4>
+            <h3 class= "driver-lastname team_{{ $teamdriver->seasonTeam->team->id }}--text">
+                {{ $teamdriver->driver->Lastname }}</h3>
         </div>
     </div>
     <div class="driver-photo">
-        <img src="{{asset('Images/Drivers/Side-profil/'.$teamdriver->driver->Lastname.".png")}}" alt="">
-        <img src="{{asset('Images/Teams/'.$teamdriver->seasonTeam->team->Name.' black.png')}}" class="driver-team" src="" alt="">
+        @if (file_exists(public_path(
+                    'Images/Drivers/Side-profil/' .
+                        $teamdriver->seasonteam->season_id .
+                        '/' .
+                        $teamdriver->driver->Lastname .
+                        '.png')))
+            <img src="{{ asset('Images/Drivers/Side-profil/' . $teamdriver->seasonteam->season_id . '/' . $teamdriver->driver->Lastname . '.png') }}"
+                alt="">
+        @else
+            <img src="{{ asset('Images/Drivers/Side-profil/' . $teamdriver->seasonteam->season_id - 1 . '/' . $teamdriver->driver->Lastname . '.png') }}"
+                alt="">
+        @endif
+        <img src="{{ asset('Images/Teams/' . $teamdriver->seasonTeam->team->Name . ' black.png') }}" class="driver-team"
+            src="" alt="">
     </div>
 </div>
-
-
-<style>
-
-    </style>
