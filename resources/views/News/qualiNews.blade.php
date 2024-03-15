d985922f41e458bbe694980ede1bc601eac72b85
Leandre <leandre.b.boislard@gmail.com>
2024-03-14 21:16:19 -0400

    Add new style to quali sprint and ss

--- resources/views/News/qualiNews.blade.php
+++ resources/views/News/qualiNews.blade.php
@@ -2,7 +2,7 @@
     $quali = $qualiStory->qualification;
     $grandPrixName = $qualiStory->qualification->grandPrixWeekend->name;
     $countryName = $quali->grandPrixWeekend->country->name;
-    $year=$qualiStory->qualification->grandPrixWeekend->season_id;
+    $year = $qualiStory->qualification->grandPrixWeekend->season_id;
 
 @endphp
 
@@ -10,21 +10,23 @@
 @extends('layouts.main')
 
 @section('content')
-    <main id="quali_story">
+    <link rel="stylesheet" href="{{ asset('css/news.css') }}">
+    <main id="quali_story" class="story">
         {{-- Intro --}}
         <section class="intro">
-            <div class="quali_intro">
+            <div class="intro_presentation">
                 <h1>{{ $qualiStory->catchphrase }}</h1>
                 <p>{{ $qualiStory->intro }}</p>
-
-
-
             </div>
-            <img src="{{ asset('Images/Stories/'. $year .'/'.'Qualifications/Main/'. $grandPrixName .'.jpg') }}" alt="Main Photo">
+            <img src="{{ asset('Images/Stories/' . $year . '/' . 'Qualifications/Main/' . $grandPrixName . '.jpg') }}"
+                alt="Main Photo">
         </section>
         {{-- Q1 --}}
-        <section class="q1 quali_content">
-            <p>{{ $qualiStory->q1 }}</p>
+        <section class="q1 article_content">
+            <div class="content-texts">
+                <h3>Q1</h3>
+                <p>{{ $qualiStory->q1 }}</p>
+            </div>
             <div class="q1Out quali_outs">
                 @foreach ($quali->q1out as $qualiresult)
                     <div class="q1_times quali_times">
@@ -50,10 +52,9 @@
                 @endforeach
                 <h1 class="out_title">Q1 out</h1>
             </div>
-            <h1 class="content_title">Q1</h1>
         </section>
         {{-- q2 --}}
-        <section class="q2 quali_content">
+        <section class="q2 article_content">
             <div class="q2Out quali_outs">
                 @foreach ($quali->q2out as $qualiresult)
                     <div class="q2_times quali_times">
@@ -79,12 +80,17 @@
                 @endforeach
                 <h1 class="out_title out_q2">Q2 out</h1>
             </div>
-            <p>{{ $qualiStory->q2 }}</p>
-            <h1 class="content_title">Q2</h1>
+            <div class="content-texts">
+                <h3>Q2</h3>
+                <p>{{ $qualiStory->q2 }}</p>
+            </div>
         </section>
         {{-- q3 --}}
-        <section class="q3 quali_content">
-            <p>{{ $qualiStory->q3 }}</p>
+        <section class="q3 article_content">
+            <div class="content-texts">
+                <h3>Q3</h3>
+                <p>{{ $qualiStory->q3 }}</p>
+            </div>
             <div class="q3Out quali_outs">
                 @foreach ($quali->q3results as $qualiresult)
                     <div class="q3_times quali_times">
@@ -110,14 +116,15 @@
                 @endforeach
                 <h1 class="out_title out_q2">Q3 resultats</h1>
             </div>
-            <h1 class="content_title">Q3</h1>
         </section>
 
         {{-- conclusion --}}
-        <section class="quali_conclusion">
+        <section class="conclusion">
             <div class="conclusion_photo">
-                <img src="{{ asset('Images/Stories/'. $year .'/'.'Qualifications/End/' . $grandPrixName .'.jpg') }}" alt="Photo Winner">
+                <img src="{{ asset('Images/Stories/' . $year . '/' . 'Qualifications/End/' . $grandPrixName . '.jpg') }}"
+                    alt="Photo Winner">
             </div>
+            <h2>En Conclusion</h2>
             <p>{{ $qualiStory->conclusion }}</p>
 
             @if ($quali->grandPrixWeekend->race->race_story_id)
@@ -134,284 +141,20 @@
             <div class="quali_visual--infos">
                 <button class="start--animation">Démarrer</button>
                 <h3>{{ $quali->winner->q3 }} s</h3>
-                <h3 class="driver_pole team_{{ $quali->winner->driver->teamDriver[0]->seasonTeam->team->id}}--text">
+                <h3 class="driver_pole team_{{ $quali->winner->driver->teamDriver[0]->seasonTeam->team->id }}--text">
                     {{ $quali->winner->driver->Firstname }} {{ $quali->winner->driver->Lastname }}</h3>
             </div>
         </section>
 
-        @if($qualiStory->extra)
-        <section class="quali_extra">
-            <h2>Mise a Jour</h2>
-            <p>{{$qualiStory->extra}}</p>
-        </section>
+        @if ($qualiStory->extra)
+            <section class="extra">
+                <h2>Mise a Jour</h2>
+                <p>{{ $qualiStory->extra }}</p>
+            </section>
         @endif
 
 
 
 
     </main>
-
-    <style>
-        #quali_story {
-            display: flex;
-            flex-direction: column;
-            padding: 2em;
-            gap: 1em;
-        }
-
-        .intro {
-            display: flex;
-            flex-wrap: wrap;
-            min-height: calc(100vh - 138px);
-        }
-
-        .quali_intro {
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            padding: 20px;
-            gap: 20px;
-            justify-content: center;
-        }
-
-
-
-        .quali_intro h1 {
-            color: var(--accent);
-            text-decoration: underline;
-            text-decoration-color: var(--text);
-            font-size: 96px;
-
-        }
-
-        #quali_story p {
-            font-size: 24px;
-        }
-
-        .intro img {
-            flex: 3;
-            object-fit: cover;
-            aspect-ratio: 16/9;
-            min-width: 300px;
-            border-radius: 50px;
-            box-shadow: var(--card-shadow);
-        }
-
-        .quali_content {
-            position: relative;
-            display: flex;
-            flex-wrap: wrap-reverse;
-            padding: 64px 1em;
-            gap: 1em;
-            margin-top: 1em;
-            align-items: center;
-        }
-
-        .quali_content p {
-            flex: 2;
-            min-width: 525px;
-            padding: 1em
-        }
-
-        .quali_content img {
-            width: 90%;
-            height: 90%;
-
-        }
-
-        .middle_part {
-            flex-direction: row-reverse;
-
-        }
-
-        .middle_part div {}
-
-        .middle_part img {
-            object-fit: cover;
-            width: 100%;
-            height: 100%;
-            border-radius: inherit;
-
-        }
-
-        .quali_outs {
-            background-color: #1E1E1E;
-            display: flex;
-            flex-direction: column;
-            box-shadow: var(--card-shadow);
-        }
-
-        .quali_times {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .quali_times:not(:last-child) {
-            border-bottom: solid 2px var(--background-light)
-        }
-
-        .quali_outs {
-            position: relative;
-            padding: 45px 1.5em;
-            flex: 1.25;
-            border-radius: 15px;
-            gap: 15px;
-        }
-
-        .out_title {
-            position: absolute;
-            left: 16px;
-            top: -16px;
-            font-size: 32px;
-            color: var(--accent)
-        }
-
-        .content_title {
-            position: absolute;
-            left: 32px;
-            top: -32px;
-            font-size: 64px;
-            color: var(--accent)
-        }
-
-        .quali_conclusion {
-            padding: 0 2em;
-            display: flex;
-            flex-direction: column;
-            gap: 1em;
-            margin: 5em 0
-        }
-
-        .quali_extra {
-            padding: 4rem;
-            border: solid var(--attention) 2px;
-            margin: 2rem;
-            border-radius: 1rem;
-            box-shadow: 0px 0px 1rem 0px var(--attention) ;
-        }
-
-        .quali_extra h2{
-            font-size: 3.2rem;
-            color: var(--attention);
-        }
-
-
-        .conclusion_photo {
-
-            display: grid;
-            place-items: center;
-        }
-
-        .quali_conclusion p {
-            padding: 0 2em;
-        }
-
-        .conclusion_photo img {
-            width: 100%;
-            max-width: 1500px;
-            aspect-ratio: 16/9;
-            object-fit: cover;
-            border-radius: 15px;
-            object-position: top;
-            box-shadow: var(--card-shadow);
-        }
-
-        .race_link {
-            font-size: 24px;
-            font-weight: 800;
-            text-decoration: none;
-            color: var(--accent);
-            width: fit-content;
-        }
-
-        .race_link::after {
-            display: block;
-            content: "";
-            width: 0%;
-            height: 5px;
-            background-color: var(--accent);
-            transition: .5s
-        }
-
-        .race_link:hover::after {
-            width: 100%;
-        }
-
-        .quali_visual {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            font-size: 32px;
-            color: var(--accent);
-            background-color: #1E1E1E;
-            align-self: center;
-            max-width: 1000px;
-            width: 100%;
-            box-shadow: var(--card-shadow);
-
-
-            border-radius: 15px;
-        }
-
-        .quali_visual button {
-            margin-top: 15px
-        }
-
-        .start--animation {
-            color: #000000;
-            font-size: 20px;
-            font-weight: 600;
-            padding: 20px;
-            background-color: var(--accent);
-            border: transparent;
-            cursor: pointer;
-            display: block;
-            margin: auto;
-            border-radius: 15px;
-        }
-
-        .start--animation:active {
-            transform: translateY(3px)
-        }
-
-        .quali_visual--infos {
-            background-color: #0F0F0F;
-            align-self: stretch;
-            border-radius: 0 0 10px 10px;
-            padding: 0 15px 15px 15px;
-            border-top: solid 2px var(--background-light)
-        }
-
-
-        @media screen and (max-width: 675px) {
-            #quali_story p {
-                font-size: 16px
-            }
-
-            .quali_intro h1 {
-                font-size: 64px
-            }
-
-            .quali_content p {
-                min-width: auto;
-            }
-
-            .quali_content div {
-                flex: auto;
-                min-width: 200px;
-            }
-        }
-
-        @media screen and (max-width: 450px) {
-
-            .quali_intro h1 {
-                font-size: 48px
-            }
-
-
-
-
-        }
-    </style>
 @endsection
--- resources/views/News/raceNews.blade.php
+++ resources/views/News/raceNews.blade.php
@@ -8,9 +8,10 @@
 @extends('layouts.main')
 
 @section('content')
-    <main id="race_story">
+    <link rel="stylesheet" href="{{ asset('css/news.css') }}">
+    <main id="race_story" class="story">
         <section class="intro">
-            <div class="race_intro">
+            <div class="intro_presentation">
                 <h1>{{ $raceStory->catchphrase }}</h1>
                 <p>{{ $raceStory->intro }}</p>
                 <a class="quali_link" href="{{ route('qualificationNews', ['id' => $raceStory->id]) }}">Qualifications -></a>
@@ -18,43 +19,43 @@
             <img src="{{ asset('Images/Stories/' . $year . '/' . 'Races/Main/' . $grandPrixName . '.jpg') }}"" alt="">
         </section>
 
-        <section class="first_part race_content">
-            <div class="race_content-photo">
+        <section class="first_part article_content">
+            <div class="content-photo">
                 <img src="{{ asset('Images/Stories/' . $year . '/' . 'Races/FirstPart/' . $grandPrixName . '.jpg') }}""
                     alt="">
             </div>
-            <div class="race_content-texts">
+            <div class="content-texts">
                 <h3>Départ</h3>
                 <p>{{ $raceStory->first_part }}</p>
             </div>
 
         </section>
 
-        <section class="middle_part race_content">
-            <div class="race_content-photo">
+        <section class="middle_part article_content">
+            <div class="content-photo">
                 <img src="{{ asset('Images/Stories/' . $year . '/' . 'Races/MiddlePart/' . $grandPrixName . '.jpg') }}""
                     alt="">
             </div>
-            <div class="race_content-texts">
+            <div class="content-texts">
                 <h3>Millieu</h3>
                 <p>{{ $raceStory->middle_part }}</p>
             </div>
 
         </section>
 
-        <section class="last_part race_content">
-            <div class="race_content-photo">
+        <section class="last_part article_content">
+            <div class="content-photo">
                 <img src="{{ asset('Images/Stories/' . $year . '/' . 'Races/LastPart/' . $grandPrixName . '.jpg') }}""
                     alt="">
             </div>
-            <div class="race_content-texts">
+            <div class="content-texts">
                 <h3>Fin</h3>
                 <p>{{ $raceStory->last_part }}</p>
             </div>
 
         </section>
 
-        <section class="race_conclusion">
+        <section class="conclusion">
             <div class="conclusion_photo">
                 <img src="{{ asset('Images/Stories/' . $year . '/' . 'Races/End/' . $grandPrixName . '.jpg') }}""
                     alt="">
@@ -65,7 +66,7 @@
 
 
         @if ($raceStory->extra)
-            <section class="race_extra">
+            <section class="extra">
                 <h2>Mise a Jour</h2>
                 <p>{{ $raceStory->extra }}</p>
             </section>
@@ -88,251 +89,4 @@
 
 
     </main>
-
-    <style>
-        #race_story {
-            display: flex;
-            flex-direction: column;
-            padding: 1em;
-        }
-
-        .intro {
-            display: flex;
-            flex-wrap: wrap;
-            min-height: calc(100vh - 138px);
-            padding: 2rem;
-            margin-bottom: 10rem;
-        }
-
-        .race_intro {
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            padding: 20px;
-            gap: 20px;
-            justify-content: center;
-        }
-
-
-
-        .race_intro h1 {
-            color: var(--accent);
-            text-decoration: underline;
-            text-decoration-color: var(--text);
-            font-size: clamp(4.8rem, 5dvw, 9.6rem)
-        }
-
-        #race_story p {
-            font-size: clamp(1.2rem, 1.5dvw, 2.4rem)
-        }
-
-        .intro img {
-            flex: 3 0 75rem;
-            object-fit: cover;
-            aspect-ratio: 16/9;
-            min-width: 300px;
-            border-radius: 0.5rem;
-            box-shadow: var(--card-shadow);
-            max-width: 100%;
-            max-height: 74.5rem;
-
-        }
-
-        .race_content {
-            display: flex;
-            flex-wrap: wrap-reverse;
-            padding: 5rem;
-            gap: 2rem;
-            margin-top: 1em;
-            align-items: center;
-            min-height: 50dvh;
-            margin-inline: auto;
-            max-width: 175rem;
-            justify-content: center;
-            background-color: var(--background-light);
-
-            @media screen and (max-width: 675px) {
-                padding: 2.5rem;
-            }
-        }
-
-        .race_content .race_content-texts {
-            flex: 1 0 30rem;
-            padding: 3.5rem;
-
-            & h3 {
-                font-size: clamp(2.4rem, 3.5dvw, 4.4rem);
-                color: var(--accent);
-            }
-
-            @media screen and (max-width: 500px) {
-                padding: 0;
-            }
-        }
-
-        .race_content .race_content-photo {
-            flex: 1 0 50rem;
-            background-color: rgba(0, 0, 0, 0.50);
-            border-radius: .5rem;
-            aspect-ratio: 16/9;
-            place-items: center;
-            align-self: stretch;
-            max-height: 100%;
-            max-width: 100%;
-            width: 100%;
-
-
-            @media screen and (max-width: 1175px) {
-                max-width: 75rem;
-                flex: 1 1 auto;
-
-
-            }
-
-
-
-        }
-
-        .race_content img {
-            object-fit: cover;
-            width: 100%;
-            height: 100%;
-            border-radius: inherit;
-            box-shadow: var(--card-shadow);
-            min-width: 25rem;
-        }
-
-        .middle_part {
-            flex-direction: row-reverse;
-
-        }
-
-
-
-
-        .quali_link {
-            font-size: 25px;
-            font-weight: 800;
-            text-decoration: none;
-            color: var(--accent);
-            width: fit-content;
-        }
-
-        .quali_link::after {
-            display: block;
-            content: "";
-            width: 0%;
-            height: 5px;
-            background-color: var(--accent);
-            transition: .5s
-        }
-
-        .quali_link:hover::after {
-            width: 100%;
-        }
-
-        .race_conclusion {
-            display: flex;
-            flex-direction: column;
-            gap: 2rem;
-            margin: 5em 0;
-            margin-inline: auto;
-            max-width: 150rem;
-            padding: 5rem;
-            background-color: var(--background-light);
-
-
-
-
-            & h2 {
-                text-decoration-color: var(--text);
-                font-size: clamp(2.4rem, 5dvw, 4.8rem);
-                color: var(--accent);
-
-            }
-
-            & .conclusion_photo {
-                display: grid;
-                place-items: center;
-
-                & img {
-                    width: 100%;
-                    aspect-ratio: 16/9;
-                    object-fit: cover;
-                    border-radius: .5rem;
-                    box-shadow: var(--card-shadow);
-                }
-
-
-            }
-        }
-
-
-        .race_extra {
-            padding: 4rem;
-            border: solid var(--attention) 2px;
-            margin: 2rem;
-            border-radius: 1rem;
-            box-shadow: 0px 0px 1rem 0px var(--attention);
-        }
-
-        .race_extra h2 {
-            font-size: 3.2rem;
-            color: var(--attention);
-        }
-
-
-
-        .race_conclusion p {
-            padding: 0 5rem;
-
-            @media screen and (max-width: 500px) {
-                padding: 0;
-            }
-        }
-
-
-
-
-        .links {
-            width: 100%;
-            display: flex;
-            flex-direction: column;
-            align-items: flex-end;
-            padding: 0 3rem
-        }
-
-        .link {
-            font-size: 25px;
-            font-weight: 800;
-            text-decoration: none;
-            color: var(--accent);
-            width: fit-content;
-        }
-
-        .link::after {
-            display: block;
-            content: "";
-            width: 0%;
-            height: 5px;
-            background-color: var(--accent);
-            transition: .5s
-        }
-
-        .link:hover::after {
-            width: 100%;
-        }
-
-        @media screen and (max-width: 675px) {
-
-            .race_content p {
-                min-width: auto;
-            }
-
-            .race_content div {
-                flex: auto;
-                min-width: 200px;
-            }
-        }
-    </style>
 @endsection
