5ed1c120a050a112cdeb804b7679a04c4bd1466c
LeandreWeb <124205610+LeandreWeb@users.noreply.github.com>
2025-01-06 20:51:04 -0500

    Fix issue with 2025

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -8,18 +8,28 @@ use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
-    public function team ($teamName){
-        
-        $year = date('Y') ;
+    public function team($teamName)
+    {
 
-        $seasonTeam=SeasonTeam::with("team")
-        ->where('season_id', $year)
-        ->whereHas('team', function($query) use ($teamName){
-            $query->where('Name',$teamName);
-        })->first();
+        $year = date('Y');
 
-        
+        $seasonTeam = SeasonTeam::with("team")
+            ->where('season_id', $year)
+            ->whereHas('team', function ($query) use ($teamName) {
+                $query->where('Name', $teamName);
+            })->first();
 
-        return view("Team.TeamPage",compact('seasonTeam'));
+        while (is_null($seasonTeam)) {
+            $year = $year - 1;
+            $seasonTeam = SeasonTeam::with("team")
+                ->where('season_id', $year)
+                ->whereHas('team', function ($query) use ($teamName) {
+                    $query->where('Name', $teamName);
+                })->first();
+        }
+
+
+
+        return view("Team.TeamPage", compact('seasonTeam'));
     }
 }
